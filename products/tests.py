0c089993a53edfcb693356fe7ee6b46abf2f4534
root <root@LAPTOP-6R83AARJ.localdomain>
2020-05-20 20:07:50 -0500

    Failed TDD

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Dress
 
-# Create your tests here.
+class ProductTest(TestCase):
+
+    def setUp(self):
+        self.parameters = {
+            'marca': 'Monstruon',
+            'size': 'small',
+            'color': 'pink',
+        }
+
+    def test_output(self):
+        output_answer = Dress(self.parameters['marca'], self.parameters['size'], self.parameters['color'])
+        self.assertEqual = (output_answer, 'Monstruon', 'small', 'pink') 
+        
+    def tearDown(self):
+            del(self.parameters)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Users
 
-# Create your tests here.
+
+class UserTest(TestCase):
+
+    def setUp(self):
+        self.parameters = {
+            'name': 'Jose',
+            'surname': 'jose123',
+            'password': 'seguro',
+            'address': 'Montevideo 5'
+        }
+
+    def test_output(self):
+        output_answer = Users(self.parameters['name'], self.parameters['surname'], self.parameters['password'], self.parameters['address'])
+        self.assertEqual = (output_answer, 'Jose', 'jose123', 'seguro', 'Montevideo 5') 
+        
+    def tearDown(self):
+            del(self.parameters)
