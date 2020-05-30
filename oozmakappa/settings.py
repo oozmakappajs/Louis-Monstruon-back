84275a7c893a50148bf57d7a212e3b21c8779eb7
Kristian Lopez <kristian.lom@gmail.com>
2020-05-30 15:14:18 -0500

    [ADD] Add enviroment variables

--- oozmakappa/settings.py
+++ oozmakappa/settings.py
@@ -55,6 +55,10 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ORIGIN_WHITELIST = [
     "louis-monstruon.now.sh",
     "oozmakappa-api.herokuapp.com",
