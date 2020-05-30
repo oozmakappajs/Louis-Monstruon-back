d4a2c18362d97d34ca59d1d92a1a8eb5637ea7cb
Jorge Diaz <jorgdiaz.lara@gmail.com>
2020-05-29 20:13:20 -0500

    Categories, filter and cart empty endpoints

--- products/urls.py
+++ products/urls.py
@@ -12,5 +12,20 @@ urlpatterns = [
         r'^api/v1/products/$',
         views.get_post_products,
         name='get_post_products'
-    )
+    ),
+    url(
+        r'^api/v1/products/filter',
+        views.get_filter,
+        name='get_filter'
+    ),
+    url(
+        r'^api/v1/products/categories',
+        views.get_post_categories,
+        name='get_post_categories'
+    ),
+    url(
+        r'^api/v1/products/cart',
+        views.get_post_cart,
+        name='get_post_cart'
+    ),
 ]
--- products/views.py
+++ products/views.py
@@ -92,3 +92,12 @@ def get_post_products(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get_post_categories(request):
+        pass
+
+    def get_filter(request):
+        pass
+
+    def get_post_cart(request):
+        pass
